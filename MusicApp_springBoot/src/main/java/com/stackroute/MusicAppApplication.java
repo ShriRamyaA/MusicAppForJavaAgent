c542ec336e16890b3b003a59073890070dd14d0f
Shri <rammiu96@gmail.com>
2019-03-11 16:04:36 +0530

    First commit

---
+++ MusicApp_springBoot/src/main/java/com/stackroute/MusicAppApplication.java
@@ -0,0 +1,15 @@
+package com.stackroute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class MusicAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicAppApplication.class, args);
+	}
+
+}
+
